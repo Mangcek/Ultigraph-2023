4484b0bad4fc8a608fe80321e67b69043eb3efe8
AnotherMelvin <41257543+AnotherMelvin@users.noreply.github.com>
2023-07-18 20:43:33 +0700

    About Page Init

---
+++ resources/js/Pages/About.tsx
@@ -0,0 +1,11 @@
+import { Link, Head } from '@inertiajs/react';
+import { PageProps } from '@/types';
+
+export default function About() {
+    return (
+        <>
+            <Head title="About" />
+            <h1>Hello World</h1>
+        </>
+    );
+}
