826afd12698b7926d52a8986d49719c7aa5767e7
Axel Ferdinand <axel.ferdinand.88@gmail.com>
2023-09-05 15:32:21 +0700

    Add Supergrafis in page Daftar Pre-Event

--- resources/js/Pages/DaftarPreEvent/DaftarPreEvent.jsx
+++ resources/js/Pages/DaftarPreEvent/DaftarPreEvent.jsx
@@ -5,6 +5,8 @@ import Footer from "../../Components/Footer/Footer";
 import "../../../css/DaftarPreEvent.scss";
 
 import BackButton from "../../../assets/daftar-preevent/ArrowLeft.png";
+import BungaKiri from "../../../assets/daftar-preevent/BungaKiri.svg";
+import BungaKanan from "../../../assets/daftar-preevent/BungaKanan.svg";
 
 export default function DaftarPreEvent({ auth, laravelVersion, phpVersion }){
     return (
@@ -12,6 +14,8 @@ export default function DaftarPreEvent({ auth, laravelVersion, phpVersion }){
             <Navbar color="#F37786"/>
             <Head title="Daftar Pre-Event" />
             <div className="daftar-preevent">
+                <img className="bunga-kiri" src={BungaKiri} alt="" />
+                <img className="bunga-kanan" src={BungaKanan} alt="" />
                 <div className="judul">
                     <a href="/pre-event" className="box-back">
                         <img className="back" src={BackButton} alt="" />
@@ -20,47 +24,49 @@ export default function DaftarPreEvent({ auth, laravelVersion, phpVersion }){
                         <p className="text">PENDAFTARAN PRE-EVENT</p>
                     </div>
                 </div>
-                <div className="konten">
-                    <div className="box-input">
-                        <p className="label">Nama Lengkap:</p>
-                        <input className="input" type="text" />
-                    </div>
-                    <div className="garis"></div>
-                    <div className="box-input">
-                        <p className="label">Email:</p>
-                        <div className="box-email">
-                            <p className="label-email">Pribadi</p>
-                            <input className="input-email" type="text" />
+                <form className="form">
+                    <div className="konten">
+                        <div className="box-input">
+                            <p className="label">Nama Lengkap:</p>
+                            <input className="input" type="text" />
+                        </div>
+                        <div className="garis"></div>
+                        <div className="box-input">
+                            <p className="label">Email:</p>
+                            <div className="box-email">
+                                <p className="label-email">Pribadi</p>
+                                <input className="input-email" type="text" />
+                            </div>
+                        </div>
+                        <div className="box-input">
+                            <p className="label"></p>
+                            <div className="box-email">
+                                <p className="label-email">Student *khusus mahasiswa UMN</p>
+                                <input className="input-email" type="text" />
+                            </div>
+                        </div>
+                        <div className="garis"></div>
+                        <div className="box-input">
+                            <p className="label">No. Telepon:</p>
+                            <input className="input" type="text" />
+                        </div>
+                        <div className="garis"></div>
+                        <p className="label-box">*khusus mahasiswa UMN</p>
+                        <div className="box-input input-3">
+                            <p className="label">NIM:</p>
+                            <input className="input" type="text" />
+                        </div>
+                        <div className="box-input input-3">
+                            <p className="label">Angkatan:</p>
+                            <input className="input" type="text" />
+                        </div>
+                        <div className="box-input input-3">
+                            <p className="label">Jurusan:</p>
+                            <input className="input" type="text" />
                         </div>
                     </div>
-                    <div className="box-input">
-                        <p className="label"></p>
-                        <div className="box-email">
-                            <p className="label-email">Student *khusus mahasiswa UMN</p>
-                            <input className="input-email" type="text" />
-                        </div>
-                    </div>
-                    <div className="garis"></div>
-                    <div className="box-input">
-                        <p className="label">No. Telepon:</p>
-                        <input className="input" type="text" />
-                    </div>
-                    <div className="garis"></div>
-                    <p className="label-box">*khusus mahasiswa UMN</p>
-                    <div className="box-input input-3">
-                        <p className="label">NIM:</p>
-                        <input className="input" type="text" />
-                    </div>
-                    <div className="box-input input-3">
-                        <p className="label">Angkatan:</p>
-                        <input className="input" type="text" />
-                    </div>
-                    <div className="box-input input-3">
-                        <p className="label">Jurusan:</p>
-                        <input className="input" type="text" />
-                    </div>
-                </div>
-                <button className="submit">SUBMIT</button>
+                    <button className="submit">SUBMIT</button>
+                </form>
             </div>
             <Footer />
         </>
